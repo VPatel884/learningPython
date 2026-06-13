a8d72e596e1dccae0ad68e4ddc1109d18bc58de8
Vikash Patel <vpatel884@gmail.com>
2026-06-13 18:51:49 +0530

    variables and expression

--- Chapter0/firstProgram.py
+++ Chapter0/firstProgram.py
@@ -1,1 +1,5 @@
 print("Hello World")
+
+# Write "Hello WOrld" 10 times in next line
+
+print(("Hello World" + "\n") * 10)
---
+++ Chapter1/variables.py
@@ -0,0 +1,19 @@
+# Valid Variables name
+name = "Ram"
+age = 45
+fathers_name = "Raghav"
+college = "NIT"
+degree_1 = "B.Tech"
+degree_2 = "M. Tech"
+
+# Invalid Variables name
+# 1age -- variable can not start with number
+# age? -- variable can not contain spacial character i.e. ?&%$ etc
+# 1age -- variable can not start with number
+
+# Taking user input
+
+userName = input("Enter your name: ")
+userAge = input("Enter your age: ")
+print("Your good name is: ", userName)
+print(userName + "'s age is ", userAge)
