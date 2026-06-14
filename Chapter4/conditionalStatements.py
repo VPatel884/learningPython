0cb87ae93f133c69f91edbf8e24f0563411f83e1
Vikash Patel <vpatel884@gmail.com>
2026-06-14 17:26:10 +0530

    Conditional Statements

---
+++ Chapter4/conditionalStatements.py
@@ -0,0 +1,23 @@
+# Conditional Statements
+
+# if/else and if/elif
+
+age = 18
+
+if age > 18:
+    print("You are eligible to vote.")
+else :
+    print("You are not eligible to vote.")
+
+marks = 40
+if marks >= 90:
+    print("Your grade is A.")
+elif 80 <= marks < 90:
+    print("Your grade is B.")
+elif 70 <= marks < 80:
+    print("Your grade is C.")
+elif 60 <= marks < 70:
+    print("Your grade is D.")
+else :
+    print("Your grade is E. Improve your marks.")
+
---
+++ Chapter4/practice1.py
@@ -0,0 +1,10 @@
+# Check whether the entered number is negative or positive
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("It's Zero")
