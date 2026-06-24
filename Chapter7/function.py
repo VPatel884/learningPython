dc71695e18ffec25602892da2cba0b89e0c0c03f
Vikash Patel <vpatel884@gmail.com>
2026-06-24 23:24:40 +0530

    Return Statement in Python

--- Chapter7/function.py
+++ Chapter7/function.py
@@ -4,4 +4,13 @@ def sumFunc(a,b) :
     sum = a + b
     print(sum)
 
-sumFunc(5, 7)
+sumFunc(5, 7) # Function Calling
+
+# Return Statement
+
+def multiply(a, b) :
+    return a * b
+
+print(multiply(5, 9))
+result = multiply(3,6)
+print(result)
--- Chapter7/practice1.py
+++ Chapter7/practice1.py
@@ -6,9 +6,45 @@ def welcome_message() :
 welcome_message()
 
 # Problem 2
+# greeting("Vikash") // Function can not be called before defining it
 
 def greeting(name) :
     print(f"Good Morning {name}!")
 
 greeting("Vikash")
 greeting("Vikram")
+
+# Problem 3
+
+def square(num) :
+    return num ** 2
+
+print(square(3))
+
+# Problem 4
+
+def convert_to_upper(word) :
+    return word.upper()
+
+print(convert_to_upper("apple"))
+
+# Takes string as input and returns number vowels and consonants
+
+def count_vow_Conso(str) :
+    vowels = "aeiouAEIOU"
+
+    countVow = 0
+    countConso = 0
+
+    for letter in str :
+        if(letter.isalpha()) :
+            if(letter in vowels) :
+                countVow += 1
+            else :
+                countConso += 1
+
+    return countVow, countConso
+
+vowNum, consoNum = count_vow_Conso("Vikash Patel")
+print(f"Number of vowels is {vowNum}")
+print(f"Number of consonants is {consoNum}")
