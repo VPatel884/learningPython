d196dc540c8ba830fcf60f92e71eff78929f2ce4
Vikash Patel <vpatel884@gmail.com>
2026-06-26 19:10:15 +0530

    Methods in OOP

--- Chapter9/classes.py
+++ Chapter9/classes.py
@@ -3,10 +3,15 @@
 # Class Creation
 
 class MyVehicle:
+    model = "Audi V8"
     color = "Black" #attribute
     fuelType = "Diesel"
     mileage = 17
 
+    def myFirstCar(self): # Function inside class is called methods
+        print("My First Car is", self.model)
+
 # Object Creation
 
 car = MyVehicle()
+car.myFirstCar()
